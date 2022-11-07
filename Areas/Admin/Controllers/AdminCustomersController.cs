038c99815a7c3c6a95f52e019c347d2c94a93f33
nartnahn1604 <20520672@gm.uit.edu.vn>
2022-11-07 23:35:17 +0700

    Admin accounts and products

---
+++ Areas/Admin/Controllers/AdminCustomersController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using IS220_PROJECT.Models;
+using PagedList.Core;
+
+namespace IS220_PROJECT.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin")]
+    [Route("[controller]/[action]")]
+    public class AdminCustomersController : Controller
+    {
+        private readonly dbFrameContext _context;
+
+        public AdminCustomersController(dbFrameContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/AdminCustomers
+        public async Task<IActionResult> Index(int? page)
+        {
+            var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            var pageSize = 20;
+            var lsCustomers = _context.Customers.AsNoTracking().OrderByDescending(x => x.CreateDate);
+            PagedList<Customer> models = new PagedList<Customer>(lsCustomers, pageNumber, pageSize);
+            ViewBag.CurrentPage = pageNumber;
+            //var dbFrameContext = _context.Customers.Include(c => c.Account);
+            return View(models);
+        }
+
+        // GET: Admin/AdminCustomers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Customers == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .Include(c => c.Account)
+                .FirstOrDefaultAsync(m => m.CustomerId == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // GET: Admin/AdminCustomers/Create
+        public IActionResult Create()
+        {
+            ViewData["AccountId"] = new SelectList(_context.Accounts, "AccountId", "AccountId");
+            return View();
+        }
+
+        // POST: Admin/AdminCustomers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CustomerId,FullName,Birthday,Avatar,Address,Email,Phone,CreateDate,Active,AccountId")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(customer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AccountId"] = new SelectList(_context.Accounts, "AccountId", "AccountId", customer.AccountId);
+            return View(customer);
+        }
+
+        // GET: Admin/AdminCustomers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Customers == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            ViewData["AccountId"] = new SelectList(_context.Accounts, "AccountId", "AccountId", customer.AccountId);
+            return View(customer);
+        }
+
+        // POST: Admin/AdminCustomers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CustomerId,FullName,Birthday,Avatar,Address,Email,Phone,CreateDate,Active,AccountId")] Customer customer)
+        {
+            if (id != customer.CustomerId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(customer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomerExists(customer.CustomerId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AccountId"] = new SelectList(_context.Accounts, "AccountId", "AccountId", customer.AccountId);
+            return View(customer);
+        }
+
+        // GET: Admin/AdminCustomers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Customers == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .Include(c => c.Account)
+                .FirstOrDefaultAsync(m => m.CustomerId == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // POST: Admin/AdminCustomers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Customers == null)
+            {
+                return Problem("Entity set 'dbFrameContext.Customers'  is null.");
+            }
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CustomerExists(int id)
+        {
+          return _context.Customers.Any(e => e.CustomerId == id);
+        }
+    }
+}
---
+++ Areas/Admin/Controllers/AdminProductsController.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using IS220_PROJECT.Models;
+using PagedList.Core;
+using System.Diagnostics;
+
+namespace IS220_PROJECT.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin")]
+    [Route("[controller]/[action]")]
+    public class AdminProductsController : Controller
+    {
+        private readonly dbFrameContext _context;
+
+        public AdminProductsController(dbFrameContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/AdminProducts
+        public async Task<IActionResult> Index(int? page)
+        {
+
+            ViewData["Cats"] = new SelectList(_context.Categories, "CatId", "CatName");
+            IEnumerable<string> _status = new string[] { "Còn hàng", "Hết hàng" };
+            ViewData["isInStock"] = new SelectList(_status);
+
+            var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            var pageSize = 20;
+            var lsProducts = _context.Products.AsNoTracking().OrderByDescending(x => x.CatId);
+            PagedList<Product> models = new PagedList<Product>(lsProducts, pageNumber, pageSize);
+            ViewBag.CurrentPage = pageNumber;
+            //var dbFrameContext = _context.Customers.Include(c => c.Account);
+            return View(models);
+        }
+
+        // GET: Admin/AdminProducts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .Include(p => p.Cat)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: Admin/AdminProducts/Create
+        public IActionResult Create()
+        {
+            ViewData["Cats"] = new SelectList(_context.Categories, "CatId", "CatName");
+            return View();
+        }
+
+        // POST: Admin/AdminProducts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductId,ProductName,Description,CatId,Price,Discount,Thumb,Video,DateCreated,DateModified,BestSellers,HomeFlag,Active,Tags,UnitsInStock")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Cats"] = new SelectList(_context.Categories, "CatId", "CatName", product.CatId);
+            return View(product);
+        }
+
+        // GET: Admin/AdminProducts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewData["Cats"] = new SelectList(_context.Categories, "CatId", "CatName", product.CatId);
+            return View(product);
+        }
+
+        // POST: Admin/AdminProducts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProductId,ProductName,Description,CatId,Price,Discount,Thumb,Video,DateCreated,DateModified,BestSellers,HomeFlag,Active,Tags,UnitsInStock")] Product product)
+        {
+            if (id != product.ProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.ProductId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Cats"] = new SelectList(_context.Categories, "CatId", "CatName", product.CatId);
+            return View(product);
+        }
+
+        // GET: Admin/AdminProducts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .Include(p => p.Cat)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Admin/AdminProducts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Products == null)
+            {
+                return Problem("Entity set 'dbFrameContext.Products'  is null.");
+            }
+            var product = await _context.Products.FindAsync(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int id)
+        {
+          return _context.Products.Any(e => e.ProductId == id);
+        }
+    }
+}
